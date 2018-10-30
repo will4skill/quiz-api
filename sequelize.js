aa4d3b6c54d0106c0bd2df4d06e0565a1677c1c6
Tim Williams <jtimwill@gmail.com>
2018-10-30 14:31:08 -0400

    Revise database tables

--- sequelize.js
+++ sequelize.js
@@ -7,7 +7,6 @@ const UserQuizModel = require('./models/user_quiz');
 const UserAnswerModel = require('./models/user_answer');
 const QuizModel = require('./models/quiz');
 const CategoryModel = require('./models/category');
-const QuizQuestionModel = require('./models/quiz_question');
 const QuestionModel = require('./models/question');
 
 const sequelize = new Sequelize('database', 'username', 'password', {
@@ -29,26 +28,24 @@ const sequelize = new Sequelize('database', 'username', 'password', {
   const UserAnswer = UserAnswerModel(sequelize, Sequelize);
   const Quiz = QuizModel(sequelize, Sequelize);
   const Category = CategoryModel(sequelize, Sequelize);
-  const QuizQuestion = QuizQuestionModel(sequelize, Sequelize);
   const Question = QuestionModel(sequelize, Sequelize);
 
 
 
   // M - M
-  User.belongsToMany(Quiz, {through: UserQuiz});
+  User.belongsToMany(Quiz, {through: UserQuiz}); // Problem: (user_id, quiz_id) UNIQUE
   Quiz.belongsToMany(User, {through: UserQuiz});
   // M - M
   UserQuiz.belongsToMany(Question, {through: UserAnswer});
   Question.belongsToMany(UserQuiz, {through: UserAnswer});
-  // M - M
-  Quiz.belongsToMany(Question, {through: QuizQuestion});
-  Question.belongsToMany(Quiz, {through: QuizQuestion});
+  // 1 - M
+  Quiz.hasMany(Question)
   // 1 - M
   Category.hasMany(Quiz);
   // 1 - M
   UserQuiz.hasMany(UserAnswer); // Confirm cascade to UA on delete
 
-  // force: true will drop the table if it already exists
+  // // force: true will drop the table if it already exists
   // sequelize.sync({ force: true }).then(() => {
   //   console.log("Database and tables created");
   //   // return User.create({
@@ -64,6 +61,5 @@ const sequelize = new Sequelize('database', 'username', 'password', {
     UserAnswer,
     Quiz,
     Category,
-    QuizQuestion,
     Question
   };
