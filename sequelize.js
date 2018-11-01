ec9c8be0602185e04adfe4dd3dd1c518d837f247
Tim Williams <jtimwill@gmail.com>
2018-11-01 13:22:26 -0400

    Prepare for deployment

--- sequelize.js
+++ sequelize.js
@@ -9,50 +9,48 @@ const QuizModel = require('./models/quiz');
 const CategoryModel = require('./models/category');
 const QuestionModel = require('./models/question');
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-  dialect: 'sqlite',
-  storage: db
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_BRONZE_URL, {
+    dialect: 'postgres',
+  });
+} else {
+  const sequelize = new Sequelize('database', 'username', 'password', {
+    dialect: 'sqlite',
+    storage: db
+  });
+}
+
+
+const User = UserModel(sequelize, Sequelize);
+const UserQuiz = UserQuizModel(sequelize, Sequelize);
+const UserAnswer = UserAnswerModel(sequelize, Sequelize);
+const Quiz = QuizModel(sequelize, Sequelize);
+const Category = CategoryModel(sequelize, Sequelize);
+const Question = QuestionModel(sequelize, Sequelize);
+
+User.hasMany(UserQuiz);
+Quiz.hasMany(UserQuiz);
+Quiz.hasMany(Question, {onDelete: 'cascade'});
+Category.hasMany(Quiz);
+UserQuiz.hasMany(UserAnswer, {onDelete: 'cascade'});
+Question.hasMany(UserAnswer);
+
+// force: true will drop the table if it already exists
+sequelize.sync().then(() => {
+  console.log("Database and tables created");
+  // return User.create({
+  //   name: 'Tim',
+  //   email: 'test@email.com',
+  //   password_digest: 'test',
+  // });
 });
 
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log('Connection has been established successfully.');
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err);
-//   });
-
-  const User = UserModel(sequelize, Sequelize);
-  const UserQuiz = UserQuizModel(sequelize, Sequelize);
-  const UserAnswer = UserAnswerModel(sequelize, Sequelize);
-  const Quiz = QuizModel(sequelize, Sequelize);
-  const Category = CategoryModel(sequelize, Sequelize);
-  const Question = QuestionModel(sequelize, Sequelize);
-
-  User.hasMany(UserQuiz);
-  Quiz.hasMany(UserQuiz);
-  Quiz.hasMany(Question, {onDelete: 'cascade'});
-  Category.hasMany(Quiz);
-  UserQuiz.hasMany(UserAnswer, {onDelete: 'cascade'});
-  Question.hasMany(UserAnswer);
-
-  // // force: true will drop the table if it already exists
-  // sequelize.sync({ force: true }).then(() => {
-  //   console.log("Database and tables created");
-  //   // return User.create({
-  //   //   name: 'Tim',
-  //   //   email: 'test@email.com',
-  //   //   password_digest: 'test',
-  //   // });
-  // });
-
-  module.exports = {
-    User,
-    UserQuiz,
-    UserAnswer,
-    Quiz,
-    Category,
-    Question,
-    sequelize
-  };
+module.exports = {
+  User,
+  UserQuiz,
+  UserAnswer,
+  Quiz,
+  Category,
+  Question,
+  sequelize
+};
